5e45868f903b63e55de60c0c71cbd07cfe7aee70
Yell0wF1sh <86252839+Yell0wF1sh@users.noreply.github.com>
2024-10-30 19:48:17 -0400

    updated sql

--- src/create-table.sql
+++ src/create-table.sql
@@ -8,35 +8,35 @@ create table if not exists languages(
     language_id             int NOT NULL,
     language_name           nvarchar(100),
     compiler_invocation     nvarchar(500),
-    create_date      DATETIME DEFAULT NOW(),
+    creation_date      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     primary key (language_id),
     UNIQUE (language_name)
 );
 
 create table if not exists projects(
     project_id       int NOT NULL,
-    owner_user_id    int,
+    owning_user_id    int,
     project_name     nvarchar(100),
     language_name    nvarchar(100),
-    create_date      DATETIME DEFAULT NOW(),
+    creation_date      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     primary key (project_id),
     foreign key (language_name) references languages (language_name)
                                    on delete set null,
-    foreign key (owner_user_id) references db_user.users (user_id)
+    foreign key (owning_user_id) references db_user.users (user_id)
                                    on delete set null
 );
 
 create table if not exists project_files(
     file_id         int NOT NULL,
     project_id      int,
-    parent_dir      int DEFAULT NULL,
+    parent_directory      int DEFAULT NULL,
     file_name       nvarchar(100),
-    is_dir          bool DEFAULT FALSE,
-    create_date      DATETIME DEFAULT NOW(),
+    is_directory          bool DEFAULT FALSE,
+    creation_date      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     primary key (file_id),
     foreign key (project_id) references projects (project_id)
                           on delete set null,
-    foreign key (parent_dir) references project_files (file_id)
+    foreign key (parent_directory) references project_files (file_id)
                           on delete set null
 );
 
